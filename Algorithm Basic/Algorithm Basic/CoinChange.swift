e2fe4872bd3c877a7f1323b2076d5e0dd9995270
Muhaimie Mazlah <laws_basic@yahoo.co.uk>
2020-01-28 03:17:41 +0900

    Coin change push file fix

--- Algorithm Basic/Algorithm Basic/CoinChange.swift
+++ Algorithm Basic/Algorithm Basic/CoinChange.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+
+func coinChange(n:Int)->Int{
+    
+    var coin = [1,2,5]
+    
+    var comb = Array(repeating: 0, count: n)
+    
+    comb.insert(1, at: 0)
+    
+    //coins value
+    for i in coin{
+        
+        //the value of n
+        for j in 1...n{
+            
+            if j >= i{
+            
+                
+                comb[j] += comb[j-i]
+                print(comb)
+            }
+            
+        }
+        print("\n")
+        
+    }
+    
+    return comb[n]
+    
+    
+}
+
+
+
